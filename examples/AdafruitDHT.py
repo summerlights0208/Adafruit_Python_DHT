12da0fccb429cb9657cee869ee9549870c44c68e
summerlights0208 <a88020812@gmail.com>
2020-11-24 23:24:05 +0800

    Changes to be committed: 	modified:   AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # Copyright (c) 2014 Adafruit Industries
 # Author: Tony DiCola
-
+#06050916測試
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
